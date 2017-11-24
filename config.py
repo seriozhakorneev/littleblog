0f345b5666d43ea2637017e9078c52bbbb75ed2b
Seriozha <seriozhakorneev@gmail.com>
2017-11-24 18:42:46 +0300

    make it better

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Configuration():
 	SQLALCHEMY_DATABASE_URI = 'mysql://sql11197214:zg2fdsHiMg@sql11.freemysqlhosting.net/sql11197214'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-	RECAPTCHA_PUBLIC_KEY = '6LfWbjcUAAAAAEB9807KCCe-nxl8NystpF3U1Kn4'
-	RECAPTCHA_PRIVATE_KEY = '6LfWbjcUAAAAANk8w5UP-76KOgiJ6bK-RaVqDtSE'
+	RECAPTCHA_PUBLIC_KEY = '6LdYQjoUAAAAAOJ6GUgcEsB_zOiExWgr5ydbRdlT'
+	RECAPTCHA_PRIVATE_KEY = '6LdYQjoUAAAAALunleE22arR7qfKU4JGhZkE55gb'
 	TESTING = False # чтобы не вводить капчу каждый раз ставить True
